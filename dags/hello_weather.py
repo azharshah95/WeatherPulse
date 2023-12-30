d5e26740664469dda4a9262e1638737d121c52ba
azharshah95 <azharshah1995@gmail.com>
2023-12-30 11:24:49 +0100

    format document

--- dags/hello_weather.py
+++ dags/hello_weather.py
@@ -33,55 +33,59 @@ def dbConnect():
 
 
 def fetchData(ti):
-    rawData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units={UNITS}").json()
-    
+    rawData = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units={UNITS}").json()
+
     ti.xcom_push(key='raw_data', value=rawData)
 
+
 def parsingRawData(ti):
-  rawData = ti.xcom_pull(key='raw_data')
-  myDict = {
-    "date": datetime.datetime.fromtimestamp(rawData["dt"]),
-    "city_name": rawData["name"],
-    "city_id": rawData["id"],
-    "country": rawData["sys"]["country"],
-    "lon": rawData["coord"]["lon"],
-    "lat": rawData["coord"]["lat"],
-    "base": rawData["base"],
-    "temp": rawData["main"]["temp"],
-    "feels_like": rawData["main"]["feels_like"],
-    "timezone": rawData["timezone"]
-  }
-  ti.xcom_push(key='parsed_data', value=myDict)
-  
+    rawData = ti.xcom_pull(key='raw_data')
+    myDict = {
+        "date": datetime.datetime.fromtimestamp(rawData["dt"]),
+        "city_name": rawData["name"],
+        "city_id": rawData["id"],
+        "country": rawData["sys"]["country"],
+        "lon": rawData["coord"]["lon"],
+        "lat": rawData["coord"]["lat"],
+        "base": rawData["base"],
+        "temp": rawData["main"]["temp"],
+        "feels_like": rawData["main"]["feels_like"],
+        "timezone": rawData["timezone"]
+    }
+    ti.xcom_push(key='parsed_data', value=myDict)
+
+
 def insertData(ti):
-  try:
-    parsedData = ti.xcom_pull(key='parsed_data')
-    conn = dbConnect()
-  
-    city_id = parsedData["city_id"],
-    lat = parsedData['lat'],
-    lon = parsedData["lon"],
-    temp = parsedData["temp"],
-    feels_like = parsedData["feels_like"],
-    city_name = parsedData["city_name"],
-    country = parsedData["country"],
-    timezone = parsedData["timezone"],
-    date = parsedData["date"],
-    
-    row = (city_id, lat, lon, temp, feels_like, city_name, country, timezone, date)      
-    SQL = "INSERT INTO tblweatherdata (city_id, lat, lon, temp, feels_like, city_name, country, timezone, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    
-    conn.run(sql=SQL, parameters=row)
+    try:
+        parsedData = ti.xcom_pull(key='parsed_data')
+        conn = dbConnect()
 
-  except (Exception) as error:
-    ti.xcom_push(key='insert_error', value=error)
-    print("Failed to insert record into tbldata", error)
+        city_id = parsedData["city_id"],
+        lat = parsedData['lat'],
+        lon = parsedData["lon"],
+        temp = parsedData["temp"],
+        feels_like = parsedData["feels_like"],
+        city_name = parsedData["city_name"],
+        country = parsedData["country"],
+        timezone = parsedData["timezone"],
+        date = parsedData["date"],
 
-  # finally:
-  #     # closing database connection.
-  #     if conn:
-  #         # conn.close()
-  #         print("PostgreSQL connection is closed")
+        row = (city_id, lat, lon, temp, feels_like,
+               city_name, country, timezone, date)
+        SQL = "INSERT INTO tblweatherdata (city_id, lat, lon, temp, feels_like, city_name, country, timezone, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
+        conn.run(sql=SQL, parameters=row)
+
+    except (Exception) as error:
+        ti.xcom_push(key='insert_error', value=error)
+        print("Failed to insert record into tbldata", error)
+
+    # finally:
+    #     # closing database connection.
+    #     if conn:
+    #         # conn.close()
+    #         print("PostgreSQL connection is closed")
 
 
 with DAG('hello_weather',
@@ -94,12 +98,12 @@ with DAG('hello_weather',
         task_id='weather_task',
         python_callable=fetchData
     )
-    
+
     transform_data = PythonOperator(
         task_id='weather_parsing_task',
         python_callable=parsingRawData
     )
-    
+
     load_data = PythonOperator(
         task_id='weather_load_task',
         python_callable=insertData
